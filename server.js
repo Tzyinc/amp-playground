360bfd1076c72ee2df6c991a9d35aa9273bc62b7
Tzyinc <tzyinc@hotmail.com>
2019-04-27 22:50:12 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+const path = require('path');
+
+// viewed at http://localhost:8080
+app.get('/', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/index.html`));
+});
+
+app.listen(8080);
